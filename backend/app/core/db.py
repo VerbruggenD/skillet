c531649dd3d611da4306019e960cbac0066b7b09
VerbruggenD <verbruggen.dieter@gmail.com>
2026-08-22 16:44:36 +0200

    fix(backend): resolve lint issues

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,12 +1,20 @@
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.environ.get('DATABASE_URL',
-                              f"postgresql://{os.environ.get('POSTGRES_USER','skillet')}:{os.environ.get('POSTGRES_PASSWORD','change-me')}@{os.environ.get('POSTGRES_HOST','postgres')}:{os.environ.get('POSTGRES_PORT','5432')}/{os.environ.get('POSTGRES_DB','skillet')}")
+DATABASE_URL = os.environ.get(
+    'DATABASE_URL',
+    (
+        f"postgresql://{os.environ.get('POSTGRES_USER', 'skillet')}:{os.environ.get('POSTGRES_PASSWORD', 'change-me')}"
+        f"@{os.environ.get('POSTGRES_HOST', 'postgres')}:{os.environ.get('POSTGRES_PORT', '5432')}"
+        f"/{os.environ.get('POSTGRES_DB', 'skillet')}"
+    ),
+)
 
 engine = create_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
 
 def get_db():
     db = SessionLocal()
